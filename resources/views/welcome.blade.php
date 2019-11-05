787768e5a0274d23c3e14a640407ccce62c70f96
eliezerk8 <gerson.peraltar@utem.cl>
2019-11-05 15:50:48 -0300

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <!--Import Google Icon Font-->
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href='https://fonts.googleapis.com/css?family=Comfortaa' rel='stylesheet'>
+        <link rel="stylesheet" href="{{asset('css/materialize.min.css')}}">
+        <style>
+            body {
+                font-family: 'Comfortaa';
+            }
+        </style>
+</head>
+    <body>
+        <nav class="light-blue lighten-1" role="navigation">
+            <div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo">Logo</a>
+                <ul class="right hide-on-med-and-down">
+                <li><a href="#">Navbar Link</a></li>
+                </ul>
+                <ul id="nav-mobile" class="sidenav">
+                <li><a href="#">Navbar Link</a></li>
+                </ul>
+                <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+            </div>
+        </nav>
+        <div class="section no-pad-bot" id="index-banner">
+            <div class="container">
+                <div class="section">
+                    <!--   Icon Section   -->
+                    <div class="row">
+                    <div class="col s12 m4">
+                      <div class="icon-block">
+                        <h2 class="center light-blue-text"><i class="material-icons">flash_on</i></h2>
+                        <h5 class="center">Speeds up development</h5>
+            
+                        <p class="light">We did most of the heavy lifting for you to provide a default stylings that incorporate our custom components. Additionally, we refined animations and transitions to provide a smoother experience for developers.</p>
+                      </div>
+                    </div>
+            
+                    <div class="col s12 m4">
+                      <div class="icon-block">
+                        <h2 class="center light-blue-text"><i class="material-icons">group</i></h2>
+                        <h5 class="center">User Experience Focused</h5>
+            
+                        <p class="light">By utilizing elements and principles of Material Design, we were able to create a framework that incorporates components and animations that provide more feedback to users. Additionally, a single underlying responsive system across all platforms allow for a more unified user experience.</p>
+                      </div>
+                    </div>
+            
+                    <div class="col s12 m4">
+                      <div class="icon-block">
+                        <h2 class="center light-blue-text"><i class="material-icons">settings</i></h2>
+                        <h5 class="center">Easy to work with</h5>
+            
+                        <p class="light">We have provided detailed documentation as well as specific code examples to help new users get started. We are also always open to feedback and can answer any questions a user may have about Materialize.</p>
+                      </div>
+                    </div>
+                </div>
+                <a class="waves-effect waves-light btn"><i class="material-icons right">cloud</i>button</a>
+            </div>
+            <br><br>
+        </div>
+        <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+        <script src="{{asset('js/materialize.min.js')}}"></script>
+        <script src="{{asset('js/mobile.js')}}"></script>
+    </body>
+</html>
